678e6572383f31bbd5de476e0ddf0b70002d42db
kostya05983 <kostya05983@mail.ru>
2019-09-08 21:49:26 +0700

    Init multy platform

---
+++ js/src/containers/ProjectsContainer.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+
+/**
+ * @author kostya05983
+ */
+class ProjectsContainer extends Component {
+
+
+    render() {
+        return (<div>
+
+            </div>
+        )
+    }
+}
